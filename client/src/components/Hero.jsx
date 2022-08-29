cd805a8f5cd3d2172d608eae23ef8e5ba5d07e70
JonatanDrysen <jojodrysen@gmail.com>
2022-08-29 21:12:37 +0200

    add components

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div>
+        <h2>Hero image with intro</h2>
+    </div>
+  )
+}
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Bio from '../components/Bio'
+import Hero from '../components/Hero'
+import Nav from '../components/Nav'
+
+export default function HomePage() {
+  return (
+    <>
+    <div>HomePage</div>
+        <Nav />
+        <Hero />
+        <Bio />
+        
+    </> 
+  )
+}
